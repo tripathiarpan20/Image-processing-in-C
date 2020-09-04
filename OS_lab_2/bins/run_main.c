59db6bc7e10cca2e30d5d6e78099a39877ed843c
Arpan <111801005@smail.iitpkd.ac.in>
2020-09-04 14:44:03 +0530

    run_main.c added

---
+++ OS_lab_2/bins/run_main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void main() {
+
+  //TODO: replace the function with 'func_name("../input/input.ppm")
+  ppm input_img = //function which takes path of the ppm image as input
+
+  //TODO: In the "prepare_main.c", this "transforms" are is supposed to be
+  //a double pointer that 
+  char* transforms[] = ["grayscale" , "blur"];
+
+  //TODO: implement a 'list transforms' command in make file that shows all
+  //the files in transforms folder
+
+  //looping through the transforms and applying on the image
+  for (int i=0; i<2; i++) {
+    if(strcmpi(transforms[i],"grayscale") == 0) {
+      matrix output = //grayscale function by pavan
+    }
+    else if(strcmpi(transforms[i],"blur")==0) {
+      ppm output = //blurring function by me
+    }
+  }
+  //The transform functions would have exceptions that occur for wrong inputs
+  
+  //Fuction to write a 'ppm' or 'matrix' structure into 'outputs/output.ppm'
+
+
+
+}
