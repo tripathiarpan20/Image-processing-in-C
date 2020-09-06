3e5ecea505caf59b7146a0805c0d96d64743c19e
pavankarthikparuchuri <70737281+pavankarthikparuchuri@users.noreply.github.com>
2020-09-06 22:12:14 +0530

    read and write_ppm updated

--- OS_lab_2/bins/ppm_util.c
+++ OS_lab_2/bins/ppm_util.c
@@ -15,7 +15,15 @@ ppm* readPPM (char* name)
   ppm *img; FILE *fp;
   int c,max;
   
-  fp = fopen(name,"rb");
+  char name1[100];
+  strcpy(name1,name);
+  strcat(name1,".ppm");
+  char fullname[100];
+  strcpy(fullname,"../inputs/");
+  strcat(fullname,name1);
+  
+ 
+  fp = fopen(fullname,"rb");
   if(!fp){
 	       fprintf(stderr,"Unable to open file\n");
 	       exit(1);
@@ -37,10 +45,10 @@ ppm* readPPM (char* name)
   fscanf(fp,"%d",&max);
   
   while(fgetc(fp)!='\n');
-  img->pxl = (pixel**)malloc(sizeof(pixel*)*img->m);
+  img->A = (pixel**)malloc(sizeof(pixel*)*img->m);
   for(int i =0;i< img->m; i++)
   {
-   *(img->pxl+i) = (pixel*)malloc(sizeof(pixel)*img->n);
+   *(img->A+i) = (pixel*)malloc(sizeof(pixel)*img->n);
   }
  
   for(int i =0;i< img->m; i++)
@@ -50,74 +58,39 @@ ppm* readPPM (char* name)
      fscanf(fp,"%d %d %d ",&img->pxl[i][j].R,&img->pxl[i][j].G,&img->pxl[i][j].B);
    }
   }
-  
   fclose(fp);
   return img;
 }
 
 
-
-
-
 void writePPM(ppm *img, char* name){
     FILE *fp;
-    printf("hi1\n");
-    //"output.ppm" for default
-    //char* filename = strcat(name,".ppm");
-    printf("hi4\n");
+    
+    char filename[100];
+    
+    strcpy(filename,name);    
+    strcat(filename,".ppm");
+   
     char* out_folder = "../outputs/";
-    printf("hi2\n");
-    //open a file for output
-    fp = fopen(name,"wb");
-    printf("hi3\n");
+    char fullname[100];
+    strcpy(fullname,out_folder);
+    strcat(fullname,filename);
+    
+    fp = fopen(fullname,"wb");
+    
     if(!fp){
         printf("Oops! Unable to open the file '%s' \n",name);
         exit(1);
     }
-    printf("hi\n");
-    //write the header of the file
-    //image format
     fprintf(fp,"P3\n");
-
-    //image size
-    fprintf(fp,"%d %d\n",img->m,img->n);
-
-    //rgb color depth
+    fprintf(fp,"%d %d\n",img->m,img->n);    
     fprintf(fp,"%d\n",255);
-
-    //printing the pixel values
     for(int i = 0 ; i < img->m ; i++){
         for(int j = 0 ; j < img->n ; j++){
            fprintf(fp,"%d %d %d ",img->pxl[i][j].R,img->pxl[i][j].G,img->pxl[i][j].B);
         }
         fprintf(fp,"\n");
     }
-
-    //fwrite(img->A,sizeof(pixel),3*img->m * img->n,fp);
-    //closing the file
     fclose(fp);
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
