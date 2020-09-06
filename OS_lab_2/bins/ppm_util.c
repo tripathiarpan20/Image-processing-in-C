2fbc13236bd8e16c29177434a98d106e7ef6d59e
pavankarthikparuchuri <70737281+pavankarthikparuchuri@users.noreply.github.com>
2020-09-06 11:30:23 +0530

    read_ppm added

--- OS_lab_2/bins/ppm_util.c
+++ OS_lab_2/bins/ppm_util.c
@@ -8,6 +8,79 @@ implement 'main' files
 Makefile
 
 */
+
+ppm* readPPM (char* name)
+{
+  char str[16];
+  ppm *img; FILE *fp;
+  int c,max;
+  
+  fp = fopen(name,"rb");
+  if(!fp){
+	       fprintf(stderr,"Unable to open file\n");
+	       exit(1);
+         }
+  fgets(str,sizeof(str),fp);
+  if(str[0]!='P' || str[1]! = '3') 
+  {
+    fprintf(stderr,"invalid format\n, image is not p3");
+  	}
+ img = (ppm*)malloc(sizeof(ppm));
+ c = getc(fp);
+ while(c == '#')
+ {
+  while(getc(fp)!='\n');
+  c = getc(fp);
+  ungetc(c,fp);
+  fscanf(fp,"%d %d",&img->m,&img->n);
+  fscanf(fp,"%d",&max);
+  while(fgetc(fp)!='\n');
+  img->A = (pixel**)malloc(sizeof(pixel*)*img->m);
+  for(int i =0;i< img->m; i++)
+  {
+   img->A+i = (pixel*)malloc(sizeof(pixel)*img->n);
+  }
+  
+  for(int i =0;i< img->m; i++)
+  {
+   for(int j =0;j< img->n; j++)
+   {
+     fscanf(fp,"%d %d %d ",&img->A[i][j].R,&img->A[i][j].G,&img->A[i][j].B);
+   }
+  }
+  
+  fclose(fp);
+  return img;
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 void writePPM(ppm *img, char* name){
     FILE *fp;
 
