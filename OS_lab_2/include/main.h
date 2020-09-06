d4dc7079a3bd3ce3d4ec3776b4725ff6ffd3ccb5
Arpan Tripathi <42506819+tripathiarpan20@users.noreply.github.com>
2020-09-06 16:16:20 +0530

    Update main.h

--- OS_lab_2/include/main.h
+++ OS_lab_2/include/main.h
@@ -1,6 +1,22 @@
 #include <stdlib.h>
 #include <stdin.h>
 
+#define MAX_RGB_VALUE 255
+
+typedef struct Pixel{
+  int R;
+  int G;
+  int B;
+}pixel;
+
+typedef struct ppm{
+  int m;
+  int n;
+  pixel **pxl;
+}ppm;
+
+void writePPM (ppm *img, char* name);
+ppm* readPPM (char* name);
 
 struct matrix_t {
   int m;
